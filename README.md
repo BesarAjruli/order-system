a04b6989700be22dd873dfdf72887e735a2d928e
Besar Ajruli <aquila1912@gmail.com>
2025-07-19 17:12:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# webpack-prettier-lint-template
---
+++ eslint-config.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        rules: {
+            semi: ["warn", "always"],
+            eqeqeq: ["error", "always"],
+            "no-unused-vars": "error",
+             "no-lone-blocks": "error"
+        }
+    }
+];
